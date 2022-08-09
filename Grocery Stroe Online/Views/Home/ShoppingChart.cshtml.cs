059e0fdd09a42ab0131ed4230cf631332ae45f2c
abdurrahmanoori <abdurrahmannoori10@gmail.com>
2022-08-09 07:28:30 +0430

    File not modified

---
+++ Grocery Stroe Online/Views/Home/ShoppingChart.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Grocery_Stroe_Online.Views.Home
+{
+    public class ShoppingChartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
