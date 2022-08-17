89fc5a942c88057b305d16f9d5159dd246c4f249
Abdurrahman <Abdurrahmannoori10@gmail.com>
2022-08-17 10:29:34 -0700

    Added N-Tier Architecture folders

--- Grocery Stroe Online/Models/Category.cs
+++ Grocery Stroe Online/Models/Category.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace Grocery_Stroe_Online.Models
     public class Category
     {
         [Key]
+        
         public int Id { get; set; }
         [Required(AllowEmptyStrings =false,ErrorMessage ="Name is Required!")]
         public string Name { get; set; }
